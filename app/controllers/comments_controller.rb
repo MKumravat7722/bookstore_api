9bae6ebf5772781109a5bbefe1ea6d2545ce0dfb
mkumravat <mohitkumravat22@gmail.com>
2025-09-23 03:10:12 +0000

    call comment serilizer

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,7 +5,7 @@ class CommentsController < ApplicationController
     book = Book.find(params[:book_id])
     comment = book.comments.new(body: params[:body], user: @current_user)
     if comment.save
-      render json: comment.as_json(include: { user: { only: [ :id, :name ] } }), status: :created
+      render json: CommentSerializer.new(comment).serializable_hash, status: :created
     else
       render json: { errors: comment.errors.full_messages }, status: :unprocessable_entity
     end

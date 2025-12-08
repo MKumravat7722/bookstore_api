9612a9aed36f720152a5442f7cb40dfcad650bfc
mohit <mohitkumravat22@gmail.com>
2025-12-08 10:09:53 -0800

    update credential

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:5173'
-
+    origins 'https://book-store-front-end-jlpm.onrender.com'
+    # https://book-store-front-end-jlpm.onrender.com
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
